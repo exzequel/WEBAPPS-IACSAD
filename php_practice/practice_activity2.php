bd2950cd32026827243ece1c84d42c2916463998
Exequel <exql.adzn@gmail.com>
2023-03-01 21:40:08 +0800

    ucwords = capitalize first letter of every word

--- php_practice/practice_activity2.php
+++ php_practice/practice_activity2.php
@@ -13,10 +13,13 @@
 
 $str = "My name is Khan";
 $str5 = "my name is Sonoo jaiswal";
+
 $str = strtolower($str);
 $str2 = strtoupper($str);
 $str3 = ucfirst($str);
 $str4 = lcfirst($str);
+
+$str6 = ucwords($str5);
 
 
 
@@ -25,6 +28,8 @@ echo "strtolower: " . $str . "<br/>";
 echo "strtoupper: " . $str2 . "<br/>";
 echo "ucfirst: " . $str3 . "<br/>";
 echo "lcfirst: " . $str4 . "<br/>";
+
+echo "ucwords: " . $str6 . "<br/>";
 
 
 
