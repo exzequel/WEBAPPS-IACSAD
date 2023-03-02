45b818bfe41dd5ef3425dc65c4bf6330d9d481d2
Exequel <exql.adzn@gmail.com>
2023-03-02 17:28:37 +0800

    initialize practice exercise

---
+++ php_practice/php_integers.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEBAPPS Sample</title>
+</head>
+<body>
+
+<?php
+
+$a = 123;
+var_dump($a);
+echo "<br>";
+
+$b = -123;
+var_dump($a);
+echo "<br>";
+
+$c = 123;
+var_dump($a);
+echo "<br>";
+
+?>
+    
+</body>
+</html>
