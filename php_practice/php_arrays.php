09416d59982ea69cc30360f04e36db5c7f408573
Exequel <exql.adzn@gmail.com>
2023-03-02 17:48:36 +0800

    add php_arrays content

---
+++ php_practice/php_arrays.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Arrays Practice</title>
+</head>
+<body>
+
+<?php
+// ADIZON, EXEQUEL
+
+$colors = array("Red", "Green", "Blue");
+var_dump($colors);
+echo "<br>";
+
+$color_codes = array (
+    "Red" => "#ff0000",
+    "Green" => "#00ff00",
+    "Blue" => "#0000ff"
+);
+
+var_dump($color_codes)
+;
+
+?>
+    
+</body>
+</html>
