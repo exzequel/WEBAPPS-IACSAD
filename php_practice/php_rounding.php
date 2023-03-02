e5dded0d5737b8445b1d1b60bd76e950c1335859
Exequel <exql.adzn@gmail.com>
2023-03-02 18:12:12 +0800

    add php square root

---
+++ php_practice/php_rounding.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Rounding Practice</title>
+</head>
+<body>
+
+<?php
+// ADIZON, EXEQUEL
+
+// Round fractions up
+echo ceil(4.2); // Outputs: 5
+echo ceil(9.99); // Outputs: 10
+echo ceil(-5.18); // Outputs: -5
+
+// Round fractions down
+echo floor(4.2); // Outputs: 4
+echo floor(9.99); // Outputs: 9
+echo floor(-5.18); // Outputs: -6
+
+?>
+    
+</body>
+</html>
