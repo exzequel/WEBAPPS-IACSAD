c30f2cb3d40569edc09d6386fa41925fab2ff6d9
Exequel <exql.adzn@gmail.com>
2023-03-02 18:04:28 +0800

    add php resources content

--- php_practice/php_resources.php
+++ php_practice/php_resources.php
@@ -7,6 +7,20 @@
     <title>PHP Resources Practice</title>
 </head>
 <body>
-    
+
+<?php
+// ADIZON, EXEQUEL
+
+// Open a file for reading
+$handle = fopen("note.txt", "r");
+var_dump($handle);
+echo "<br>";
+
+//Connect to MySQL database server with default setting
+$link = mysqli_connect("localhost", "root", "");
+var_dump($link);
+
+?>
+   
 </body>
 </html>
