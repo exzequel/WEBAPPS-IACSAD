5b634634fbaf16bcd643f1bc150046d7a2a22223
Exequel <exql.adzn@gmail.com>
2023-03-02 17:40:17 +0800

    add php boolean content

---
+++ php_practice/php_boolean.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Boolean Practice</title>
+</head>
+<body>
+
+<?php
+// ADIZON, EXEQUEL
+
+// Assigns the value TRUE to a variable
+$show_error = true;
+var_dump($show_error);
+
+?>
+    
+</body>
+</html>s
