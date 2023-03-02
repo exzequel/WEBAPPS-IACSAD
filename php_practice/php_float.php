6647270eeb49b417348fb0cf02dc0dfa9e28c038
Exequel <exql.adzn@gmail.com>
2023-03-02 17:36:06 +0800

    initialize php float practice

--- php_practice/php_float.php
+++ php_practice/php_float.php
@@ -4,27 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WEBAPPS Practice Exercise</title>
+    <title>PHP Float Practice</title>
 </head>
 <body>
 
 <?php
 // ADIZON, EXEQUEL
 
-$a = 123; // decimal number
-var_dump($a);
-echo "<br>";
 
-$b = -123; // negative number
-var_dump($b);
-echo "<br>";
-
-$c = 0x1A; // hexadecimal number
-var_dump($c);
-echo "<br>";
-
-$d = 0123; // octal number
-var_dump($d);
 
 ?>
     
