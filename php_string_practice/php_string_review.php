296592574988a5f24641c41a6d44f33c60ab90a2
Exequel <exql.adzn@gmail.com>
2023-03-08 18:58:03 +0800

    add php string review for exam

---
+++ php_string_practice/php_string_review.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+$php_founder = "Rasmus Lerdorf";
+$php_date = 1994;
+$php2 = "Personal Home Page Tools";
+$php2_date = "November 1, 1997";
+$php3 = "PHP: Hypertext Preprocessor";
+$php3_date = "June 6, 1998";
+$php3_devs = "Zeev Suraski and Andi Gutmans students of Technion - Israel
+Institute of Technology";
+$php4 = "Zend Engine";
+$php4_date = "May 22, 2000";
+$php5 = "Zend Engine II";
+$php5_date = "July 13, 2004";
+$php5_news = "Support for PHP 5 has been discontinued since 10 Jan 2019";
+$php7_date = "December 17, 2015";
+$php8 = "PHP 8.0 released November 26, 2020";
+
+echo $php_founder . " founded PHP in " . $php_date;
+echo "<br> PHP was originally developed to keep track of who was looking at Rasmus Lerdorf's online resume.";
+
+echo "<br> PHP 2.0 was released in " . $php2_date . " and was originally called as " . $php2;
+echo "<br> PHP 3.0 renamed to $php3 was released in $php3_date and developed by $php3_devs";
+echo "<br> PHP 4.0 called the $php4 was released in $php4_date with an estimated 4 million users";
+echo "<br> PHP 5.0 or the $php5 was released in $php5_date and became an Open Source Project";
+echo "<br> $php5_news";
+echo "<br> PHP 7.0 was released in $php7_date";
+echo "<br> $php8";
+
+?>
+
+</body>
+</html>
