b3c6ff85d811f0dde799fc6e9d0aa6bdd1446a73
Exequel <exql.adzn@gmail.com>
2023-03-16 12:35:10 +0800

    update php loop practice notes

---
+++ loop_practice/04_example.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Factorial</title>
+    <link rel="stylesheet" href="04_example.css">
+</head>
+
+<body>
+    <div class="container">
+        <form method="post">
+            <label for="factorial">Enter a number to find its factorial: </label>
+            <input type="number" required name="factorial">
+            <button type="submit" class="btn">Calculate</button>
+        </form>
+
+        <?php
+        // ADIZON, EXEQUEL B.
+        if (isset($_POST['factorial'])) {
+            $factorial = $_POST['factorial'];
+
+            // For Loop
+            $result = 1;
+            for ($i = $factorial; $i > 1; $i--) {
+                $result *= $i;
+            }
+            echo "Factorial of $factorial using for loop: $result<br>";
+
+            // While Loop
+            $result = 1;
+            $i = $factorial;
+            while ($i > 1) {
+                $result *= $i;
+                $i--;
+            }
+            echo "Factorial of $factorial using while loop: $result<br>";
+
+            // Do-while Loop
+            $result = 1;
+            $i = $factorial;
+            do {
+                $result *= $i;
+                $i--;
+            } while ($i > 1);
+            echo "Factorial of $factorial using do-while loop: $result";
+        }
+        ?>
+    </div>
+</body>
+
+</html>
--- loop_practice/php_loop.php
+++ loop_practice/php_loop.php
@@ -1,10 +1,53 @@
 <?php
 
     // while loop
-    $count = 0;
-    while ($count <= 10){
-        echo $count . " ";
-        $count++;
-    }
+    // $count = 0;
+    // while ($count <= 10){
+    //     echo $count . " ";
+    //     $count++;
+    // }
+
+    // $x = 1;
+    // while ($x <= 5){
+    //     echo "The number is: $x <br>";
+    //     $x++;
+    // }
+
+    // $count = 0;
+    // do {
+    //     echo $count . ' ';
+    //     $count++;
+    // } while($count <= 10);
+
+    // $x = 1;
+    // do {
+    //     echo "The number is: $x <br>";
+    //     $x++;
+    // } while ($x <= 5);
+
+    // for ($count=0; $count <= 10; $count++) {
+    //     echo $count . ' ,';
+    // }
+
+    // for ($x = 0; $x <= 10; $x++) {
+    //     echo "The number is: $x <br>";
+    // }
+
+    // $prime = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    // foreach ($prime as $value) {
+    //     echo $value . ",";
+    // }
+
+    // $primes = array(3=>2, 5=>3, 7=>5, 9=>7, 11=>11, 13=>13, 17=>17);
+    
+    // foreach ($primes as $location => $prime){
+    //     echo $location . ":" . $prime . "<br>";
+    // }
+
+    // $colors = array("red", "green", "blue", "yellow");
+
+    // foreach($colors as $value){
+    //     echo "$value <br>";
+    // }
 
 ?>
