050c10f5fe5a4fa13ad037f9a2caccb6a079d609
Exequel <exql.adzn@gmail.com>
2023-04-03 11:58:02 +0800

    fix formatting

--- loops_hands_on/program_4.php
+++ loops_hands_on/program_4.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Program 4: Diamond</title>
 </head>
-
 <body>
     <form action="program_4.php" method="post">
         <label for="rows">Enter an odd number for the diamond size (1-19):</label>
@@ -48,5 +46,4 @@
     }
     ?>
 </body>
-
 </html>
